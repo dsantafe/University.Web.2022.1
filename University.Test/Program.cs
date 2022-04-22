55f9226a10f18f72148526be9427e19835fd0469
David Santafe <santafedavid01@gmail.com>
2022-04-21 21:08:08 -0500

    CLASE 2022-04-21 FINAL

--- University.Test/Program.cs
+++ University.Test/Program.cs
@@ -21,27 +21,47 @@ namespace University.Test
             //foreach (var item in courses2)
             //{
             //    Console.WriteLine($"{item.Title} {item.Credits}");
-            //}
-
-            //Mostrar en consola los 3 libros con más ventas.
-            //Mostrar en consola los 3 libros con menos ventas.
-            //Mostrar en consola el autor con más libros publicados.
-            //Mostrar en consola el autor y la cantidad de libros publicados.
-            //Mostrar en consola los libros publicados hace menos de 50 años.
-            //Mostrar en consola el libro más viejo.
-            //Mostrar en consola los autores que tengan un libro que comience con 'El'.
+            //}            
 
             var books = Book.Books();
             var authors = Author.Authors();
 
-            //Linq
+            //Mostrar en consola los 3 libros con más ventas.
             var ex1 = books.OrderByDescending(x => x.Sales).Take(3).ToList();
+
+            //Mostrar en consola los 3 libros con menos ventas.
             var ex2 = books.OrderBy(x => x.Sales).Take(3).ToList();
 
-            foreach (var item in ex2)
+            //Mostrar en consola el autor con más libros publicados.
+            var ex3 = from b in books
+                      join a in authors on b.AuthorId equals a.AuthorId
+                      group a by (a.AuthorId, a.Name) into query
+                      orderby query.Count() descending
+                      select query;
+
+            var resultEx3 = ex3.FirstOrDefault();
+            Console.WriteLine($"{resultEx3.Key.AuthorId} - {resultEx3.Key.Name} - {resultEx3.Count()}");
+            Console.WriteLine();
+
+            //Mostrar en consola el autor y la cantidad de libros publicados.
+            foreach (var item in ex3)
             {
-                Console.WriteLine($"{item.Title} - {item.Sales}");
+                Console.WriteLine($"{item.Key.Name} - {item.Count()}");
             }
+
+            //Mostrar en consola los libros publicados hace menos de 50 años.
+            var ex4 = books.Where(x => x.PublicationDate > DateTime.Now.AddYears(-50).Year).ToList();
+
+            //Mostrar en consola el libro más viejo.
+            var ex5 = books.OrderBy(x => x.PublicationDate).FirstOrDefault();
+
+            #region Mostrar en consola los autores que tengan un libro que comience con 'El'.
+            //Mostrar en consola los autores que tengan un libro que comience con 'El'.
+            var ex6 = from b in books
+                      join a in authors on b.AuthorId equals a.AuthorId
+                      where b.Title.StartsWith("El")
+                      select a; 
+            #endregion
 
             Console.ReadKey();
         }
